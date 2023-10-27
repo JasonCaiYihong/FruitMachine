5d83be53c1035fa1a6cbec8e3cb8ad2fa0cd84c7
JasonCaiYihong <2481335601@qq.com>
2023-10-27 21:48:40 +0800

    basic functions

--- program/src/Driver.java
+++ program/src/Driver.java
@@ -1,10 +1,17 @@
 import java.util.Scanner;
+import java.util.Random;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Driver
 {
     int qty = 0;//The quantity of users
     int size = 10;//The maximum of users
-    String[] StoredID = new String[size];
-    String[] StoredPwd = new String[size];
+    int currUser = 0;//The current user
+    String[] storedID = new String[size];
+    String[] storedPwd = new String[size];
+    int[] storedScore = new int[size];
+    int[] storedPlays = new int[size];
     Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args)
@@ -25,9 +32,11 @@ public class Driver
             boolean match = false;
             for(int t = 0; t < size; t++)
             {
-                if((ID.equals(StoredID[t]))&&(pwd.equals(StoredPwd[t])))
+                if((ID.equals(storedID[t]))&&(pwd.equals(storedPwd[t])))
                 {
                     match = true;
+                    currUser = t;
+                    //System.out.println(t);
                 }
             }
             if(match)
@@ -46,15 +55,15 @@ public class Driver
                 System.out.println("You have " + (2 - i) + " more chances.");
             }
         }
-
+        play();
     }
 
     private void register()
     {
         System.out.println("Please set your ID: ");
-        StoredID[qty] = sc.nextLine();
+        storedID[qty] = sc.nextLine();
         System.out.println("Please set your password: ");
-        StoredPwd[qty]  = sc.nextLine();
+        storedPwd[qty]  = sc.nextLine();
         //System.out.println(StoredID[qty] + StoredPwd[Qty] + qty);
         qty++;
     }
@@ -84,7 +93,7 @@ public class Driver
                     case "b" -> register();
                     default -> System.out.println("Invalid option entered: " + option);
                 }
-                System.out.println("\nPress enter key to continue...");
+                System.out.println("\nPress enter to continue...");
                 sc.nextLine();
                 option = mainMenu();
             }
@@ -98,6 +107,56 @@ public class Driver
         }
         System.out.println("Exiting...bye");
         System.exit(0);
+    }
+
+    private void FruitMachine()
+    {
+
+    }
+
+    private String select()
+    {
+        Random rand = new Random();
+        List<String> patterns = new ArrayList<>();
+        patterns.add("apple");
+        patterns.add("banana");
+        patterns.add("grape");
+        patterns.add("orange");
+        patterns.add("lemon");
+        patterns.add("cash");
+        int index = rand.nextInt(patterns.size());
+        String pattern = patterns.get(index);
+        return pattern;
+    }
+
+
+    private int game()
+    {
+        int score = 0;
+        String a = select();
+        String b = select();
+        String c = select();
+        if((a.equals("cash"))&&(b.equals("cash"))&&(c.equals("cash")))
+        {
+            score = 5;
+        }
+        else if((a.equals(b))&&(a.equals(c)))
+        {
+            score = 3;
+        }
+        else if((a.equals(b))||(a.equals(c))||(b.equals(c)))
+        {
+            score = 1;
+        }
+        return score;
+    }
+
+    private void play()
+    {
+        System.out.println("Hello, " + storedID[currUser] + "! press enter to continue...");
+        sc.nextLine();
+        storedScore[currUser] = game();
+        System.out.println("You get " + storedScore[currUser] + " in this round!");
     }
 }
 
