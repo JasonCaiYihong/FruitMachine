3c71c2e161b2746ac8a6214308ac84c3a1ea24a3
JasonCaiYihong <2481335601@qq.com>
2023-12-17 17:45:29 +0800

    delete repeated lines

--- program/src/User.java
+++ program/src/User.java
@@ -1,6 +1,3 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
 import java.util.Scanner;
 
 public class User {
@@ -115,45 +112,15 @@ public class User {
         System.exit(0);
     }
 
-    private String select() {
-        Random rand = new Random();
-        List<String> patterns = new ArrayList<>();
-        patterns.add("apple");
-        patterns.add("banana");
-        patterns.add("grape");
-        patterns.add("orange");
-        patterns.add("lemon");
-        patterns.add("cash");
-        int index = rand.nextInt(patterns.size());
-        String pattern = patterns.get(index);
-        return pattern;
-    }
-
-
-    private int game() {
-        int score = 0;
-        String a = select();
-        String b = select();
-        String c = select();
-        System.out.println(a + " " + b + " " + c);
-        if ((a.equals("cash")) && (b.equals("cash")) && (c.equals("cash"))) {
-            score = 5;
-        } else if ((a.equals(b)) && (a.equals(c))) {
-            score = 3;
-        } else if ((a.equals(b)) || (a.equals(c)) || (b.equals(c))) {
-            score = 1;
-        }
-        return score;
-    }
-
-    private void play() {
+        private void play() {
+        FruitMachine fruitMachine = new FruitMachine();
         System.out.println("One coin can be used for one round. Please enter the number of coins: ");
         transCoin = sc.nextLine();
         int coin = Integer.parseInt(transCoin);//Transfer String to int
         storedPlays[currUser] += coin;
         int score = 0;
         for (int i = 0; i < coin; i++) {
-            score += game();
+            score += fruitMachine.game();
         }
         storedScore[currUser] += score;
         System.out.println("You get " + score + " !");
